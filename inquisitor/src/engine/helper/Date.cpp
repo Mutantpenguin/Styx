1e7dccbb3563bb1e6bf7089f1d1a7bdcbac1433d
Markus Lobedann <markus.lobedann@gmail.com>
2016-12-29 11:30:57 +0100

    more const

--- inquisitor/src/engine/helper/Date.cpp
+++ inquisitor/src/engine/helper/Date.cpp
@@ -7,8 +7,8 @@ namespace Date
 {
 	std::string GetCurrentDateTimeString( void )
 	{
-		std::chrono::system_clock::time_point p = std::chrono::system_clock::now();
-		std::time_t tm = std::chrono::system_clock::to_time_t( p );
+		const std::chrono::system_clock::time_point p = std::chrono::system_clock::now();
+		const std::time_t tm = std::chrono::system_clock::to_time_t( p );
 
 		std::ostringstream oss;
 		oss << std::put_time( std::localtime( &tm ), "%Y-%m-%d_%H:%M:%S" );
