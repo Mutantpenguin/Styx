bf1fd3f196272c73c5fee97f35a80c52efae975e
Markus Lobedann <markus.lobedann@gmail.com>
2017-01-05 23:10:13 +0100

    use fmt to format string in GetCurrentDateTimeString

--- inquisitor/src/engine/helper/Date.cpp
+++ inquisitor/src/engine/helper/Date.cpp
@@ -1,18 +1,16 @@
 #include "Date.hpp"
 
 #include <chrono>
-#include <iomanip>
+
+#include "src/ext/fmt/time.h"
 
 namespace Date
 {
 	std::string GetCurrentDateTimeString( void )
 	{
 		const std::chrono::system_clock::time_point p = std::chrono::system_clock::now();
-		const std::time_t tm = std::chrono::system_clock::to_time_t( p );
+		const std::time_t time = std::chrono::system_clock::to_time_t( p );
 
-		std::ostringstream oss;
-		oss << std::put_time( std::localtime( &tm ), "%Y-%m-%d_%H:%M:%S" );
-
-		return( oss.str() );
+		return( fmt::format( "{0:%Y-%m-%d_%H:%M:%S}", fmt::localtime( time ) ) );
 	}
 }
