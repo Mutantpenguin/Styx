fc34811b899274af4611e2191e1d4efa037927b3
Markus Lobedann <markus.lobedann@gmail.com>
2017-04-24 20:46:46 +0200

    more const

--- inquisitor/src/engine/math/Math.cpp
+++ inquisitor/src/engine/math/Math.cpp
@@ -4,17 +4,17 @@
 
 namespace Math
 {
-	bool IsPowerOfTwo( std::int32_t number )
+	bool IsPowerOfTwo( const std::int32_t number )
 	{
 		return( ( number & ( number - 1 ) ) == 0 );
 	}
 
-	float frand( float min, float max )
+	float frand( const float min, const float max )
 	{
 		return( min + ( static_cast< float >( rand() ) / static_cast< float >( RAND_MAX ) ) * ( max - min ) );
 	}
 
-	std::int32_t irand( std::int32_t min, std::int32_t max )
+	std::int32_t irand( const std::int32_t min, const std::int32_t max )
 	{
 		return( min + (rand()%(max-min+1) ) );
 	}
--- inquisitor/src/engine/math/Math.hpp
+++ inquisitor/src/engine/math/Math.hpp
@@ -6,13 +6,13 @@
 namespace Math
 {
 	/** Checks if "number" is a power of two; zero and one are powers of two too. */
-	bool	IsPowerOfTwo( std::int32_t number );
+	bool	IsPowerOfTwo( const std::int32_t number );
 
 	/** Creates a random float between min and max. */
-	float	frand( float min, float max );
+	float	frand( const float min, const float max );
 
 	/** Creates a random 32bit integer between min and max. */
-	std::int32_t	irand( std::int32_t min, std::int32_t max );
+	std::int32_t	irand( const std::int32_t min, const std::int32_t max );
 }
 
 #endif // MATH_HPP
