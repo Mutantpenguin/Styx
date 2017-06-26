b706caa7ef743853333d7bfd09f31f8f8d6c76be
Markus Lobedann <markus.lobedann@gmail.com>
2017-06-26 19:01:28 +0200

    pass by const reference

--- inquisitor/src/engine/renderer/model/CMesh.cpp
+++ inquisitor/src/engine/renderer/model/CMesh.cpp
@@ -270,7 +270,7 @@ void CMesh::CalculateModelMatrix( void )
 
 glm::vec3 CMesh::CalculatedBoundingSphereRadiusVector( const Primitives::SPrimitive &primitive )
 {
-	return( (*std::max_element( std::cbegin( primitive.Vertices ), std::cend( primitive.Vertices ), []( Primitives::SVertex a, Primitives::SVertex b ){ return( glm::length2( a.Position ) > glm::length2( b.Position ) ); } ) ).Position );
+	return( (*std::max_element( std::cbegin( primitive.Vertices ), std::cend( primitive.Vertices ), []( const Primitives::SVertex &a, const Primitives::SVertex &b ){ return( glm::length2( a.Position ) > glm::length2( b.Position ) ); } ) ).Position );
 }
 
 float CMesh::CalculatedBoundingSphereRadius( const glm::vec3 &radiusVector, const glm::vec3 &scale )
