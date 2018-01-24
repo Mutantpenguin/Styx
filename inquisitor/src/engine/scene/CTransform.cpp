c14fc486c05f24dcc53514a9cfae027340da3f1c
Markus Lobedann <markus.lobedann@gmail.com>
2018-01-24 21:50:52 +0100

    transform is not a component anymore, every entity has one

---
+++ inquisitor/src/engine/scene/CTransform.cpp
@@ -0,0 +1,48 @@
+#include "src/engine/scene/components/CTransform.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include "src/engine/scene/CWorld.hpp"
+
+void CTransform::Position( const glm::vec3 &position )
+{
+	m_position = position;
+}
+
+const glm::vec3 &CTransform::Position( void ) const
+{
+	return( m_position );
+}
+
+void CTransform::Orientation( const glm::quat &orientation )
+{
+	m_orientation = orientation;
+}
+
+void CTransform::Scale( const glm::vec3 &scale )
+{
+	m_scale = scale;
+}
+
+const glm::vec3 &CTransform::Scale( void ) const
+{
+	return( m_scale );
+}
+
+const glm::quat &CTransform::Orientation( void ) const
+{
+	return( m_orientation );
+}
+
+void CTransform::Rotate( const float pitchAngle, const float yawAngle, const float rollAngle )
+{
+	m_orientation = glm::angleAxis( glm::radians( pitchAngle ), CWorld::X ) * m_orientation;
+	m_orientation = glm::angleAxis( glm::radians( yawAngle ), CWorld::Y ) * m_orientation;
+	m_orientation = glm::angleAxis( glm::radians( rollAngle ), CWorld::Z ) * m_orientation;
+}
+
+const glm::mat4 CTransform::ViewMatrix( void ) const
+{
+	return( glm::translate( glm::toMat4( m_orientation ), -m_position ) );
+}
