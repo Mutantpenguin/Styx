15234f40cbb193727febd78fbad061697c454a14
Markus Lobedann <markus.lobedann@gmail.com>
2018-01-28 23:07:19 +0100

    changed name of parameter

--- inquisitor/src/engine/scene/CFrustum.hpp
+++ inquisitor/src/engine/scene/CFrustum.hpp
@@ -10,7 +10,7 @@ class CFrustum
 public:
 	explicit CFrustum( const glm::mat4 &viewProjectionMatrix );
 
-	bool IsSphereInside( const glm::vec3 &position, const float boundingSphereRadius ) const;
+	bool IsSphereInside( const glm::vec3 &position, const float sphereRadius ) const;
 
 private:
 	std::array< CPlane, 6 > m_planes;
