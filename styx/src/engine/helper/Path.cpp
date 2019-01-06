d21d350f9360449a2506a6cf8e9d9526b99cfbb1
Markus Lobedann <markus.lobedann@gmail.com>
2019-01-06 23:54:25 +0100

    use chars instead of strings

--- styx/src/engine/helper/Path.cpp
+++ styx/src/engine/helper/Path.cpp
@@ -4,16 +4,16 @@ namespace Path
 {
 	std::string Extension( const std::string &path )
 	{
-		return( path.substr( path.find_first_of( "." ) + 1 ) );
+		return( path.substr( path.find_first_of( '.' ) + 1 ) );
 	}
 	
 	std::string Directory( const std::string &path )
 	{
-		return( path.substr( 0, path.find_last_of( "/" ) + 1 ) );
+		return( path.substr( 0, path.find_last_of( '/' ) + 1 ) );
 	}
 	
 	std::string Name( const std::string &path )
 	{
-		return( path.substr( path.find_last_of( "/" ) + 1 ) );
+		return( path.substr( path.find_last_of( '/' ) + 1 ) );
 	}
 };
