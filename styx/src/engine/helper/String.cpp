7ea16b0c12412c4bb9449d086fe0cb2bf66b0fbe
Markus Lobedann <markus.lobedann@gmail.com>
2019-01-06 23:43:25 +0100

    trim TAB, CR and LF too

--- styx/src/engine/helper/String.cpp
+++ styx/src/engine/helper/String.cpp
@@ -4,8 +4,8 @@ namespace String
 {
 	std::string trim( const std::string& str )
 	{
-		const std::string::size_type first = str.find_first_not_of( ' ' );
-		const std::string::size_type last  = str.find_last_not_of( ' ' );
+		const std::string::size_type first = str.find_first_not_of( " \t\r\n" );
+		const std::string::size_type last  = str.find_last_not_of( " \t\r\n" );
 
 		return( str.substr( first, ( last - first + 1 ) ) );
 	}
