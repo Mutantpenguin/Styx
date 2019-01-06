ed03df6f835b2f8656b5aa1e7353ca1fc0f7f9da
Markus Lobedann <markus.lobedann@gmail.com>
2019-01-06 23:02:23 +0100

    update version

--- styx/src/engine/system/CEngine.cpp
+++ styx/src/engine/system/CEngine.cpp
@@ -8,8 +8,8 @@
 #include "src/engine/states/CStateIntro.hpp"
 
 const std::string	CEngine::m_name				{ "Styx Engine" };
-const u16			CEngine::m_version_major	{ 18 };
-const u16			CEngine::m_version_minor	{ 8 };
+const u16			CEngine::m_version_major	{ 19 };
+const u16			CEngine::m_version_minor	{ 1 };
 const u16			CEngine::m_version_patch	{ 0 };
 const std::string	CEngine::m_status			{ "pre-alpha" };
 
