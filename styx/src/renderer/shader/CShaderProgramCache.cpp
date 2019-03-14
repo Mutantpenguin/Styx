6628d1b12c1c13428874069287b3e5274f38dd74
Markus Lobedann <markus.lobedann@gmail.com>
2019-03-14 13:44:41 +0100

    former version was bullshit wince we always get back a shader. in the worst case it's a dummy

--- styx/src/renderer/shader/CShaderProgramCache.cpp
+++ styx/src/renderer/shader/CShaderProgramCache.cpp
@@ -2,31 +2,15 @@
 
 void CShaderProgramCache::Load( const std::shared_ptr< CShaderProgram > &resource, const CShaderProgram::ResourceIdType &id ) const
 {
-	const auto vertexShader = m_resourceCacheManager.Get<CShader>( id.vertexShader );
-	const auto fragmentShader = m_resourceCacheManager.Get<CShader>( id.fragmentShader );
+	resource->VertexShader = m_resourceCacheManager.Get<CShader>( id.vertexShader );
+	resource->FragmentShader = m_resourceCacheManager.Get<CShader>( id.fragmentShader );
 
-	if( ( nullptr == vertexShader )
-		||
-		( nullptr == fragmentShader ) )
+	if( !m_shaderProgramCompiler.Compile( resource ) )
 	{
-		logWARNING( "using dummy shader instead of vertex shader '{0}' and fragment shader '{1}'", id.vertexShader, id.fragmentShader );
+		logWARNING( "program object from vertex shader '{0}' and fragment shader '{1}' is not valid", id.vertexShader, id.fragmentShader );
 
 		resource->VertexShader = m_shaderCompiler.DummyVertexShader();
 		resource->FragmentShader = m_shaderCompiler.DummyFragmentShader();
 		m_shaderProgramCompiler.Compile( resource );
-	}
-	else
-	{
-		resource->VertexShader = vertexShader;
-		resource->FragmentShader = fragmentShader;
-
-		if( !m_shaderProgramCompiler.Compile( resource ) )
-		{
-			logWARNING( "program object from vertex shader '{0}' and fragment shader '{1}' is not valid", id.vertexShader, id.fragmentShader );
-
-			resource->VertexShader = m_shaderCompiler.DummyVertexShader();
-			resource->FragmentShader = m_shaderCompiler.DummyFragmentShader();
-			m_shaderProgramCompiler.Compile( resource );
-		}
 	}
 }
