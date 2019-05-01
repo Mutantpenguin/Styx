6e2d81e62cbc4d70b97b586547df2ca1adfdcc2a
Markus Lobedann <markus.lobedann@gmail.com>
2019-05-01 22:55:50 +0200

    remove commented code

--- styx/src/renderer/CBO.cpp
+++ styx/src/renderer/CBO.cpp
@@ -1,14 +1,5 @@
 #include "CBO.hpp"
 
-/*
-CBO::CBO( const size_t size, const void * data )
-{
-	glCreateBuffers( 1, &GLID );
-
-	glNamedBufferData( GLID, size, data, GL_STATIC_DRAW );
-}
-*/
-
 CBO::~CBO()
 {
 	if( glIsBuffer( GLID ) == GL_TRUE )
