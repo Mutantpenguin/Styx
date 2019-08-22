e4a48b21cc41059cd05325131b87162f5e30e63e
Markus Lobedann <markus.lobedann@gmail.com>
2019-08-22 22:41:00 +0200

    created files for compiling stb_rect_pack and stb_truetype

---
+++ styx/external/stb/stb_rect_pack_impl.cpp
@@ -0,0 +1,6 @@
+#define STB_RECT_PACK_IMPLEMENTATION
+#include "external/stb/stb_rect_pack.h"
+
+/*
+ * this file solely exists for the purpose of compiling "stb_rect_pack"
+ */
---
+++ styx/external/stb/stb_truetype_impl.cpp
@@ -0,0 +1,8 @@
+#include "external/stb/stb_rect_pack.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "external/stb/stb_truetype.h"
+
+/*
+ * this file solely exists for the purpose of compiling "stb_truetype"
+ */
